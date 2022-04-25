9f38b272609b254fa27d43305e158574d82e6754
Your Name <you@example.com>
2022-04-25 16:22:48 +0900

    hello world出力

--- server/src/main/java/com/example/app/AppApplication.java
+++ server/src/main/java/com/example/app/AppApplication.java
@@ -2,12 +2,19 @@ package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping; // 追加
+import org.springframework.web.bind.annotation.RestController; //追加
 
 @SpringBootApplication
+@RestController // 追加
 public class AppApplication {
+
+	@RequestMapping("/") // 追加
+	public String home() { // 追加
+		return "Hello World"; // 追加
+	} // 追加
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
 	}
-
 }
