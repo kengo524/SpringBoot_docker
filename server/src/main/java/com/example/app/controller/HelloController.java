df4067ceb83cad24e506ce68f44623f7bb0bbd8c
Your Name <you@example.com>
2022-04-26 12:17:56 +0900

    readmeファイルの変更

--- server/src/main/java/com/example/app/controller/HelloController.java
+++ server/src/main/java/com/example/app/controller/HelloController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HelloController {
   @RequestMapping(value = "/", method = RequestMethod.GET)
   public String index(Model model) {
-    model.addAttribute("message", "world.");
+    model.addAttribute("message", "Hello World.");
 
     return "index";
   }
