2e19bac9efe952ddd4fe93b315d767b0247c620a
Your Name <you@example.com>
2022-04-26 17:50:55 +0900

    usersのnew画面実装完了W

--- server/src/main/java/com/example/app/controller/UserController.java
+++ server/src/main/java/com/example/app/controller/UserController.java
@@ -1,26 +1,92 @@
 package com.example.app.controller;
 
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+
+// import com.example.app.repository.UserRepository;
+
+// @Controller
+// public class UserController {
+
+// private final UserRepository repository;
+
+// @Autowired
+// public UserController(UserRepository repository) {
+// this.repository = repository;
+// }
+
+// @RequestMapping("/users")
+// public String users(Model model) {
+// model.addAttribute("users", repository.findAll());
+// return "users";
+// }
+
+// }
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.example.app.repository.UserRepository;
+import com.example.app.entity.User;
+import com.example.app.service.UserService;
 
 @Controller
+@RequestMapping("/users") // ①
 public class UserController {
-
-  private final UserRepository repository;
-
   @Autowired
-  public UserController(UserRepository repository) {
-    this.repository = repository;
+  private UserService userService;
+
+  @GetMapping
+  public String index(Model model) { // ②
+    List<User> users = userService.findAll();
+    model.addAttribute("users", users); // ③
+    return "index"; // ④
   }
 
-  @RequestMapping("/users")
-  public String users(Model model) {
-    model.addAttribute("users", repository.findAll());
-    return "users";
+  @GetMapping("new")
+  public String newUser(Model model) {
+    return "users/new";
   }
 
+  // @GetMapping("{id}/edit")
+  // public String edit(@PathVariable Long id, Model model) { // ⑤
+  // User player = userService.findOne(id);
+  // model.addAttribute("player", player);
+  // return "users/edit";
+  // }
+
+  // @GetMapping("{id}")
+  // public String show(@PathVariable Long id, Model model) {
+  // User player = userService.findOne(id);
+  // model.addAttribute("player", player);
+  // return "users/show";
+  // }
+
+  @PostMapping
+  public String create(@ModelAttribute User user) {
+    userService.save(user);
+    return "redirect:/users";
+  }
+
+  // @PutMapping("{id}")
+  // public String update(@PathVariable Long id, @ModelAttribute User player) {
+  // player.setId(id);
+  // ; return }
+
+  @DeleteMapping("{id}")
+  public String destroy(@PathVariable Long id) {
+    userService.delete(id);
+    return "redirect:/users";
+  }
 }
---
+++ server/src/main/java/com/example/app/service/UserService.java
@@ -0,0 +1,34 @@
+package com.example.app.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.app.entity.User;
+import com.example.app.repository.UserRepository;
+
+@Service
+public class UserService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+  public List<User> findAll() {
+    return userRepository.findAll();
+  }
+
+  // 保留後で実装する
+  // public User findOne(Long id) {
+  // return userRepository.findById(id);
+  // }
+
+  public User save(User user) {
+    return userRepository.save(user);
+  }
+
+  public void delete(Long id) {
+    userRepository.deleteById(id);
+  }
+
+}
